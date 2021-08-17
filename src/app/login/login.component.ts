bae82cfc3496da95dc4b8b8da1676ef522d2bcb4
Guan-Tse Wu <guan-tse.wu@griffithuni.edu.au>
2021-08-17 16:28:41 +1000

    Week 4 all tasks

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  username = '';
+  password = '';
+
+  constructor(private route: Router) {}
+
+  ngOnInit(): void {}
+
+  accountCheck() {
+    if (
+      (this.username == 'test1' && this.password == '123') ||
+      (this.username == 'test2' && this.password == '123') ||
+      (this.username == 'test3' && this.password == '123')
+    ) {
+      alert('success');
+      this.route.navigate(['/account']);
+    } else alert('User name and password do not match');
+  }
+}
